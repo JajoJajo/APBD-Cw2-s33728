3eb67addf8894d6ea916ffc270902dd7597695a3
Józef Rogulski <s33728@pjwstk.edu.pl>
2026-03-26 20:33:58 +0100

    Added IRentService.cs and its implementation

---
+++ APBD-Cw2-s33728/APBD-Cw2-s33728/Service/RentService/IRentService.cs
@@ -0,0 +1,15 @@
+﻿using APBD_Cw2_s33728.Models;
+
+namespace APBD_Cw2_s33728.Service.RentService;
+
+public interface IRentService
+{
+    void Rent(User user, Equipment equipment, int days);
+    int Return(int rentId);
+    int ForceReturnWithAdditionalDays(int rentId, int days);
+
+    List<IRent> GetUserRents(User user);
+    List<IRent> GetActiveRents();
+    List<IRent> GetOverdueRents();
+    List<IRent> GetAll();
+}
---
+++ APBD-Cw2-s33728/APBD-Cw2-s33728/Service/RentService/RentService.cs
@@ -0,0 +1,82 @@
+﻿using APBD_Cw2_s33728.Exceptions;
+using APBD_Cw2_s33728.Models;
+
+namespace APBD_Cw2_s33728.Service.RentService;
+
+public class RentService : IRentService
+{
+    private readonly List<IRent> _rents = [];
+
+    public void Rent(User user, Equipment equipment, int days)
+    {
+        if (!equipment.IsAvailable)
+            throw new EquipmentUnavailableException(equipment.Id);
+
+        int activeRents = _rents.Count(rents => rents.User == user && !rents.IsReturned);
+
+        if (activeRents >= user.GetMaxRentals())
+        {
+            throw new TooManyRentedException(activeRents, user.GetMaxRentals());
+        }
+        
+        var rent = new Rent(user, equipment, days);
+        _rents.Add(rent);
+        
+        equipment.MarkUnavailable();
+    }
+
+    public int Return(int rentId)
+    {
+        var rent = _rents.FirstOrDefault(r => r.Id == rentId);
+
+        if (rent is null)
+        {
+            throw new RentNotFoundException(rentId);
+        }
+        rent.Return();
+        rent.Equipment.MarkAvailable();
+        return CalculatePenalty(rent);
+    }
+    
+    public int ForceReturnWithAdditionalDays(int rentId, int days)
+    {
+        var rent = _rents.FirstOrDefault(r => r.Id == rentId);
+
+        if (rent is null)
+        {
+            throw new RentNotFoundException(rentId);
+        }
+        rent.ForceReturnWithAdditionalDays(days);
+        rent.Equipment.MarkAvailable();
+        return CalculatePenalty(rent);
+    }
+
+    public List<IRent> GetUserRents(User user)
+    {
+        return _rents.Where(r => r.User == user && !r.IsReturned).ToList();
+    }
+
+    public List<IRent> GetActiveRents()
+    {
+        return _rents.Where(r => !r.IsReturned).ToList();
+    }
+
+    public List<IRent> GetOverdueRents()
+    {
+        return _rents.Where(r => !r.IsReturned && DateTime.Now > r.DueDate).ToList();
+    }
+
+    public List<IRent> GetAll()
+    {
+        return _rents;
+    }
+
+    private int CalculatePenalty(IRent rent)
+    {
+        if (!rent.IsLate())
+            return 0;
+        int daysLate = (rent.ReturnDate.Value - rent.DueDate).Days;
+
+        return daysLate * 10;
+    }
+}
