187e1f1b756700b628fdf6331f7d84fec7984788
Józef Rogulski <s33728@pjwstk.edu.pl>
2026-03-26 20:35:16 +0100

    Added IEquipmentService.cs adn its implenetation to work on equipment

---
+++ APBD-Cw2-s33728/APBD-Cw2-s33728/Service/EquipmentService/EquipmentService.cs
@@ -0,0 +1,36 @@
+﻿using APBD_Cw2_s33728.Exceptions;
+using APBD_Cw2_s33728.Models;
+
+namespace APBD_Cw2_s33728.Service.EquipmentService;
+
+public class EquipmentService : IEquipmentService
+{
+    private readonly List<Equipment> _equipment = [];
+    
+    public void Add(Equipment equipment)
+    {
+        _equipment.Add(equipment);
+    }
+
+    public List<Equipment> GetAll()
+    {
+        return _equipment;
+    }
+
+    public List<Equipment> GetAvailable()
+    {
+        return _equipment.Where(e => e.IsAvailable).ToList();
+    }
+
+    public Equipment GetById(int id)
+    {
+        var equipment = _equipment.FirstOrDefault(e => e.Id == id);
+        return equipment ?? throw new EquipmentNotFoundException(id);
+    }
+
+    public void MarkUnavailable(int id)
+    {
+        var equipment = GetById(id);
+        equipment.MarkUnavailable();
+    }
+}
---
+++ APBD-Cw2-s33728/APBD-Cw2-s33728/Service/EquipmentService/IEquipmentService.cs
@@ -0,0 +1,15 @@
+﻿using APBD_Cw2_s33728.Models;
+
+namespace APBD_Cw2_s33728.Service.EquipmentService;
+
+public interface IEquipmentService
+{
+    void Add(Equipment equipment);
+
+    List<Equipment> GetAll();
+    List<Equipment> GetAvailable();
+
+    Equipment GetById(int id);
+
+    void MarkUnavailable(int id);
+}
