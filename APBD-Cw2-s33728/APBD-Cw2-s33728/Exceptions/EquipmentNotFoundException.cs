8b3a6a9d165cee25a4ccf3e3169946900498a0fc
Józef Rogulski <s33728@pjwstk.edu.pl>
2026-03-26 20:28:26 +0100

    Added excepitons EquipmentNotFoundException.cs and UserNotFoundException.cs

---
+++ APBD-Cw2-s33728/APBD-Cw2-s33728/Exceptions/EquipmentNotFoundException.cs
@@ -0,0 +1,3 @@
+﻿namespace APBD_Cw2_s33728.Exceptions;
+
+public class EquipmentNotFoundException(int equipmentId) : RentException($"Equipment with ID {equipmentId} was not found.");
---
+++ APBD-Cw2-s33728/APBD-Cw2-s33728/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,3 @@
+﻿namespace APBD_Cw2_s33728.Exceptions;
+
+public class UserNotFoundException(int userId) : RentException($"User with ID {userId} was not found.");
