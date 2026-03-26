676def93539a759171ed18728af0ecaa3cc6aa7d
Józef Rogulski <s33728@pjwstk.edu.pl>
2026-03-26 20:30:51 +0100

    Added interface and its implementation Rent

---
+++ APBD-Cw2-s33728/APBD-Cw2-s33728/Service/IRent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using APBD_Cw2_s33728.Models;
+
+namespace APBD_Cw2_s33728.Service;
+
+public interface IRent
+{
+    int Id { get; }
+    User User { get; }
+    Equipment Equipment { get; }
+    
+    DateTime StartDate { get; }
+    DateTime DueDate { get; }
+    DateTime? ReturnDate { get; }
+    
+    bool IsReturned { get; }
+
+    void Return();
+    void ForceReturnWithAdditionalDays(int days);
+    bool IsLate();
+}
---
+++ APBD-Cw2-s33728/APBD-Cw2-s33728/Service/Rent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using APBD_Cw2_s33728.Exceptions;
+using APBD_Cw2_s33728.Models;
+
+namespace APBD_Cw2_s33728.Service;
+
+public class Rent : IRent
+{
+    private static int _idCounter = 1;
+
+    public int Id { get; }
+    public User User { get; }
+    public Equipment Equipment { get; }
+    public DateTime StartDate { get; }
+    public DateTime DueDate { get; }
+    public DateTime? ReturnDate { get; private set; }
+
+    public bool IsReturned => ReturnDate.HasValue;
+    public void Return()
+    {
+        if (IsReturned)
+            throw new RentAlreadyReturnedException(Id);
+        ReturnDate = DateTime.Now;
+    }
+
+    public bool IsLate()
+    {
+        return IsReturned && ReturnDate > DueDate;
+    }
+
+    public Rent(User user, Equipment equipment, int days)
+    {
+        Id = _idCounter++;
+        User = user;
+        Equipment = equipment;
+        StartDate = DateTime.Now;
+        DueDate = StartDate.AddDays(days);
+    }
+    
+    public void ForceReturnWithAdditionalDays(int days)
+    {
+        if (IsReturned)
+            throw new RentAlreadyReturnedException(Id);
+        ReturnDate = DateTime.Now.AddDays(days);
+    }
+}
