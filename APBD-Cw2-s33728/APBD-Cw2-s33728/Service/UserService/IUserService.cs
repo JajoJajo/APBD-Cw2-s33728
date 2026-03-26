bb56bb84624a5c14ca966c3b50ff2e670314542c
Józef Rogulski <s33728@pjwstk.edu.pl>
2026-03-26 20:35:59 +0100

    ADded IUserService.cs and its implementation

---
+++ APBD-Cw2-s33728/APBD-Cw2-s33728/Service/UserService/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using APBD_Cw2_s33728.Models;
+
+namespace APBD_Cw2_s33728.Service.UserService;
+
+public interface IUserService
+{
+    void Add(User user);
+
+    List<User> GetAll();
+
+    User GetById(int id);
+}
---
+++ APBD-Cw2-s33728/APBD-Cw2-s33728/Service/UserService/UserService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using APBD_Cw2_s33728.Exceptions;
+using APBD_Cw2_s33728.Models;
+
+namespace APBD_Cw2_s33728.Service.UserService;
+
+public class UserService : IUserService
+{
+    private readonly List<User> _users = [];
+
+    public void Add(User user)
+    {
+        _users.Add(user);
+    }
+
+    public List<User> GetAll()
+    {
+        return _users;
+    }
+
+    public User GetById(int id)
+    {
+        var user = _users.FirstOrDefault(u => u.Id == id);
+        return user ?? throw new UserNotFoundException(id);
+    }
+}
