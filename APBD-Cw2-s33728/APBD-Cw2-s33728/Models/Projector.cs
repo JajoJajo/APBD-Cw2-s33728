e8579c9679a63a9676602773606d266b41e11dcf
Józef Rogulski <s33728@pjwstk.edu.pl>
2026-03-26 20:21:41 +0100

    Added third children class for equipment and started work on user abstract class

---
+++ APBD-Cw2-s33728/APBD-Cw2-s33728/Models/Projector.cs
@@ -0,0 +1,7 @@
+﻿namespace APBD_Cw2_s33728.Models;
+
+public class Projector(string name, int brightness, bool isPortable) : Equipment(name)
+{
+    public int Lumens { get; set; } = brightness;
+    public bool IsPortable { get; set; } = isPortable;
+}
