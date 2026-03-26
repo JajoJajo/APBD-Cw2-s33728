7b1d23ad54360ea600a69b172f74ede7b3744792
Józef Rogulski <s33728@pjwstk.edu.pl>
2026-03-26 20:37:15 +0100

    Added IReportService.cs and its implementation to help with generating reports

---
+++ APBD-Cw2-s33728/APBD-Cw2-s33728/Service/ReportService/IReportService.cs
@@ -0,0 +1,6 @@
+﻿namespace APBD_Cw2_s33728.Service.ReportService;
+
+public interface IReportService
+{
+    string GenerateSummary();
+}
---
+++ APBD-Cw2-s33728/APBD-Cw2-s33728/Service/ReportService/ReportService.cs
@@ -0,0 +1,27 @@
+﻿using APBD_Cw2_s33728.Service.EquipmentService;
+using APBD_Cw2_s33728.Service.RentService;
+
+namespace APBD_Cw2_s33728.Service.ReportService;
+
+public class ReportService(IEquipmentService equipmentService, IRentService rentService) : IReportService
+{
+    private readonly IEquipmentService _equipmentService = equipmentService;
+    private readonly IRentService _rentService = rentService;
+
+
+    public string GenerateSummary()
+    {
+        var totalEquipment = _equipmentService.GetAll().Count;
+        var availableEquipment = _equipmentService.GetAvailable().Count;
+        var activeRents = _rentService.GetActiveRents().Count;
+        var overdueRents = _rentService.GetOverdueRents().Count;
+
+        return $"""
+                ===== REPORT =====
+                Total equipment: {totalEquipment}
+                Available equipment: {availableEquipment}
+                Active rents: {activeRents}
+                Overdue rents: {overdueRents}
+                """;
+    }
+}
