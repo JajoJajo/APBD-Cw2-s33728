b0433cddf9374cc14b4845473b426b1c73291f97
Józef Rogulski <s33728@pjwstk.edu.pl>
2026-03-26 20:22:35 +0100

    ADded 2 children classes for user Employee.cs and Student.cs

---
+++ APBD-Cw2-s33728/APBD-Cw2-s33728/Models/Employee.cs
@@ -0,0 +1,6 @@
+﻿namespace APBD_Cw2_s33728.Models;
+
+public class Employee(string fName, string lName) : User(fName, lName)
+{
+    public override int GetMaxRentals() => 5;
+}
---
+++ APBD-Cw2-s33728/APBD-Cw2-s33728/Models/Student.cs
@@ -0,0 +1,6 @@
+﻿namespace APBD_Cw2_s33728.Models;
+
+public class Student (string fName, string lName) : User(fName, lName)
+{
+    public override int GetMaxRentals() => 2;
+}
